f89b1de0659b58f42d436837a2a19a77a61fde85
howard-stearns <howard.stearns@gmail.com>
2018-07-08 20:41:04 -0700

    art images, and adjust script for chappel being in our localhost

--- cityscrapes-navigation.js
+++ cityscrapes-navigation.js
@@ -1,22 +1,25 @@
 var returnButton;
 var debugLabel = 'hrs6';
-var browserID = '{3cad459f-fce0-43e2-b71a-59c8597ed483}';
+//var browserID = '{3cad459f-fce0-43e2-b71a-59c8597ed483}';
+var browserID = '{bb521a02-3d14-480c-809c-bc7c89375891}';
+print(debugLabel, 'loading');
+
+function setButton() {
+    print(debugLabel, 'button creation');
+    returnButton = Overlays.addOverlay("image", {
+        x: 100,
+        y: 350,
+        width: 100,
+        height: 100,
+        // imageURL: "http://s3.amazonaws.com/hifi-public/images/close.png",
+        imageURL: "file:///C:/Users/howar_000/Desktop/interactive-cityscrapes/back to studio button.black.png"
+    });
+}
 
 function wireButton() {
-  if (returnButton) { return; }
-  returnButton = Overlays.addOverlay("image", {
-    x: 100,
-    y: 350,
-    width: 50,
-    height: 50,
-    imageURL: "http://s3.amazonaws.com/hifi-public/images/close.png",
-    color: {
-        red: 255,
-        green: 255,
-        blue: 255
-    },
-    alpha: 1
-  });
+    print(debugLabel, 'wireButton existing:', returnButton);
+    if (returnButton) { return; }
+    setButton();
 }
 function unwireButton() {
     if (!returnButton) { return; }
@@ -24,52 +27,74 @@ function unwireButton() {
     returnButton = null;
 }
 
-var hackcount = 0;
 function mousePressEvent(event) {
-    var clickedOverlay = Overlays.getOverlayAtPoint({
-        x: event.x,
-        y: event.y
-    });
-
     // returnButton is only present when we're travelling.
+    print(debugLabel, 'mousePressEvent existing:', returnButton);
     if (returnButton) {
+        var clickedOverlay = Overlays.getOverlayAtPoint({
+            x: event.x,
+            y: event.y
+        });
+        print(debugLabel, 'clicked:', clickedOverlay, 'button:', returnButton);
         if (clickedOverlay === returnButton) {
-            print(debugLabel, hackcount, 'return button');
-            location.handleLookupString("localhost/4.43,-10.5154,27/0,1.0,0,0", false)
+            location.handleLookupString("localhost/0,0,0/0,0,0,1.0", false);
+            Script.setTimeout(function () {
+                Entities.editEntity(browserID, {"sourceUrl": "http://aubreyhaase.com/wp-content/uploads/2016/12/blog-3-pic.jpg"});
+                print(debugLabel, 'set url to', Entities.getEntityProperties(browserID, ["sourceUrl"]).sourceUrl);
+                Script.setTimeout(function () {
+                    Entities.editEntity(browserID, {"sourceUrl": "file:///C:/Users/howar_000/Desktop/interactive-cityscrapes/studio.html"});
+                    print(debugLabel, 'set url to', Entities.getEntityProperties(browserID, ["sourceUrl"]).sourceUrl);
+                }, 100);
+            }, 100);
         }
         return;
     }
-    
-    // A click on the web entity: Find out what we're zoomed into.
-    var data = Entities.getEntityProperties(browserID, 'sourceUrl');
-    print(debugLabel, hackcount, 'data', data);
-    if (!data) { return; }
-    var sourceUrl = data.sourceUrl;
-    var base = sourceUrl.slice(sourceUrl.lastIndexOf('/') + 1, -5)
-    print(debugLabel, hackcount, 'sourceUrl', sourceUrl, 'base', base);
-    if (hackcount++ > 0) base = 'national-park';
-
-    if (base === 'studio') { return; } // If we're on the studio picture, do nothing.
-    location.handleLookupString(base, false);  // Else, go to the place that matches the picture name.
 }
 
+function onLocationChangeRequired(position, hasOrientationChange, orientation, shouldFaceLocation) {
+    print(debugLabel, 'locationChangeRequired', JSON.stringify(position), hasOrientationChange, JSON.stringify(orientation), shouldFaceLocation);
+    if (location.hostname !== 'localhost') { return; }
+    if (Vec3.distance(position, {"x":0,"y":0,"z":-10}) > 0.5) { 
+        unwireButton();
+    } else {
+        wireButton();
+    }
+}
+function onPathChangeRequired(path) { print(debugLabel, 'pathChangeRequired', JSON.stringify(path)); }
+function onPossibleDomainChangeRequired(domainUrl, domainID) {
+    print(debugLabel, 'possibleDomainChangeRequired', domainUrl, domainID);
+}
+function onPossibleDomainChangeRequiredViaICEForID(hostName, domainID) {
+    print(debugLabel, 'possibleDomainChangeRequiredViaICEForID', hostName, domainID);
+}
+location.locationChangeRequired.connect(onLocationChangeRequired);
+location.pathChangeRequired.connect(onPathChangeRequired);
+location.possibleDomainChangeRequired.connect(onPossibleDomainChangeRequired);
+location.possibleDomainChangeRequiredViaICEForID.connect(onPossibleDomainChangeRequiredViaICEForID);
+
 function onLocationChange(host) {
-    hackcount = 0;
-    print(debugLabel, hackcount, 'location change', host);
+    print(debugLabel, 'location change', host);
     if (host === 'localhost') {
         unwireButton();
-        print(debugLabel, 'set hackcount', hackcount);
     } else {
         wireButton();
     }
 }
 
+setButton();
 function cleanup() {
+    print(debugLabel, 'cleanup');
     unwireButton();
     Controller.mousePressEvent.disconnect(mousePressEvent);
     location.hostChanged.disconnect(onLocationChange);
+location.locationChangeRequired.disconnect(onLocationChangeRequired);
+location.pathChangeRequired.disconnect(onPathChangeRequired);
+location.possibleDomainChangeRequired.disconnect(onPossibleDomainChangeRequired);
+location.possibleDomainChangeRequiredViaICEForID.disconnect(onPossibleDomainChangeRequiredViaICEForID);
+
 }
 
 Script.scriptEnding.connect(cleanup);
 Controller.mousePressEvent.connect(mousePressEvent);
 location.hostChanged.connect(onLocationChange);
+print(debugLabel, 'loaded');
