6ea3285de78024ea44c9d33ad37e7629b59bd87b
howard-stearns <howard.stearns@gmail.com>
2018-07-14 10:54:12 -0700

    clean up script

--- cityscrapes-navigation.js
+++ cityscrapes-navigation.js
@@ -1,6 +1,5 @@
 var returnButton;
 var debugLabel = 'hrs6';
-//var browserID = '{3cad459f-fce0-43e2-b71a-59c8597ed483}';
 var browserID = '{bb521a02-3d14-480c-809c-bc7c89375891}';
 print(debugLabel, 'loading');
 
@@ -11,7 +10,6 @@ function setButton() {
         y: 350,
         width: 100,
         height: 100,
-        // imageURL: "http://s3.amazonaws.com/hifi-public/images/close.png",
         imageURL: "file:///C:/Users/howar_000/Desktop/interactive-cityscrapes/back to studio button.black.png"
     });
 }
@@ -26,6 +24,18 @@ function unwireButton() {
     Overlays.deleteOverlay(returnButton);
     returnButton = null;
 }
+function wireButtonUnless(toBeUnwired) {
+    if (toBeUnwired) {
+        unwireButton();
+    } else {
+        wireButton();
+    }
+}
+
+function goHome() {
+    MyAvatar.resetSensorsAndBody(); // Is there a better way?
+    location.handleLookupString("localhost/0,0,0/0,0,0,1.0", false);
+}
 
 function mousePressEvent(event) {
     // returnButton is only present when we're travelling.
@@ -37,64 +47,55 @@ function mousePressEvent(event) {
         });
         print(debugLabel, 'clicked:', clickedOverlay, 'button:', returnButton);
         if (clickedOverlay === returnButton) {
-            location.handleLookupString("localhost/0,0,0/0,0,0,1.0", false);
-            Script.setTimeout(function () {
+            if (location.hostname === 'localhost') { // If we're already here, we won't be reloading, which resets the url.
                 Entities.editEntity(browserID, {"sourceUrl": "http://aubreyhaase.com/wp-content/uploads/2016/12/blog-3-pic.jpg"});
                 print(debugLabel, 'set url to', Entities.getEntityProperties(browserID, ["sourceUrl"]).sourceUrl);
                 Script.setTimeout(function () {
                     Entities.editEntity(browserID, {"sourceUrl": "file:///C:/Users/howar_000/Desktop/interactive-cityscrapes/studio.html"});
                     print(debugLabel, 'set url to', Entities.getEntityProperties(browserID, ["sourceUrl"]).sourceUrl);
+                    Script.setTimeout(goHome, 1000);
                 }, 100);
-            }, 100);
+            } else {
+                goHome();
+            }
         }
         return;
     }
 }
 
+function onHostChange(host) {
+    print(debugLabel, 'host change', host);
+    wireButtonUnless(host === 'localhost');
+}
+// Fancier version to handle easel and chappel being in the same domain.
 function onLocationChangeRequired(position, hasOrientationChange, orientation, shouldFaceLocation) {
     print(debugLabel, 'locationChangeRequired', JSON.stringify(position), hasOrientationChange, JSON.stringify(orientation), shouldFaceLocation);
     if (location.hostname !== 'localhost') { return; }
-    if (Vec3.distance(position, {"x":0,"y":0,"z":-10}) > 0.5) { 
-        unwireButton();
-    } else {
-        wireButton();
-    }
+    wireButtonUnless(Vec3.distance(position, {"x":0,"y":0,"z":-10}) > 0.5);
 }
-function onPathChangeRequired(path) { print(debugLabel, 'pathChangeRequired', JSON.stringify(path)); }
-function onPossibleDomainChangeRequired(domainUrl, domainID) {
-    print(debugLabel, 'possibleDomainChangeRequired', domainUrl, domainID);
-}
-function onPossibleDomainChangeRequiredViaICEForID(hostName, domainID) {
-    print(debugLabel, 'possibleDomainChangeRequiredViaICEForID', hostName, domainID);
-}
-location.locationChangeRequired.connect(onLocationChangeRequired);
-location.pathChangeRequired.connect(onPathChangeRequired);
-location.possibleDomainChangeRequired.connect(onPossibleDomainChangeRequired);
-location.possibleDomainChangeRequiredViaICEForID.connect(onPossibleDomainChangeRequiredViaICEForID);
+// These are for debugging.
+//function onPathChangeRequired(path) { print(debugLabel, 'pathChangeRequired', JSON.stringify(path)); }
+//function onPossibleDomainChangeRequired(domainUrl, domainID) { print(debugLabel, 'possibleDomainChangeRequired', domainUrl, domainID); }
+//function onPossibleDomainChangeRequiredViaICEForID(hostName, domainID) { print(debugLabel, 'possibleDomainChangeRequiredViaICEForID', hostName, domainID); }
+//location.pathChangeRequired.connect(onPathChangeRequired);
+//location.possibleDomainChangeRequired.connect(onPossibleDomainChangeRequired);
+//location.possibleDomainChangeRequiredViaICEForID.connect(onPossibleDomainChangeRequiredViaICEForID);
 
-function onLocationChange(host) {
-    print(debugLabel, 'location change', host);
-    if (host === 'localhost') {
-        unwireButton();
-    } else {
-        wireButton();
-    }
-}
-
-setButton();
 function cleanup() {
     print(debugLabel, 'cleanup');
     unwireButton();
     Controller.mousePressEvent.disconnect(mousePressEvent);
-    location.hostChanged.disconnect(onLocationChange);
-location.locationChangeRequired.disconnect(onLocationChangeRequired);
-location.pathChangeRequired.disconnect(onPathChangeRequired);
-location.possibleDomainChangeRequired.disconnect(onPossibleDomainChangeRequired);
-location.possibleDomainChangeRequiredViaICEForID.disconnect(onPossibleDomainChangeRequiredViaICEForID);
-
+    location.hostChanged.disconnect(onHostChange);
+    location.locationChangeRequired.disconnect(onLocationChangeRequired);
+// debugging
+//location.pathChangeRequired.disconnect(onPathChangeRequired);
+//location.possibleDomainChangeRequired.disconnect(onPossibleDomainChangeRequired);
+//location.possibleDomainChangeRequiredViaICEForID.disconnect(onPossibleDomainChangeRequiredViaICEForID);
 }
 
+setButton();
 Script.scriptEnding.connect(cleanup);
 Controller.mousePressEvent.connect(mousePressEvent);
-location.hostChanged.connect(onLocationChange);
+location.hostChanged.connect(onHostChange);
+location.locationChangeRequired.connect(onLocationChangeRequired);
 print(debugLabel, 'loaded');
