c6c7908ea140a83de341ae8a7b59955f9046f39c
howard-stearns <howard.stearns@gmail.com>
2018-07-14 15:06:46 -0700

    hostname change, and locked mapping

--- cityscrapes-navigation.js
+++ cityscrapes-navigation.js
@@ -1,7 +1,68 @@
 var returnButton;
 var debugLabel = 'hrs6';
-var browserID = '{bb521a02-3d14-480c-809c-bc7c89375891}';
-print(debugLabel, 'loading');
+var browserID = '{bb521a02-3d14-480c-809c-bc7c89375891}'; // Entity Id of the browser's Web Entity.
+var home = 'black-microchip-7727';
+
+var script = Script.resolvePath('')
+print(debugLabel, 'loading', script);
+var lockedMapping = Controller.newMapping(script + 'locked');
+var touchMapping = Controller.newMapping(script + 'touch');
+
+// Inherited:
+// Keyboard.A/D => Actions.Yaw
+// Touchscreen.DragLeft/Right => Actions.Yaw w/scale
+//
+// Keyboard.W => Actions.LONGITUDINAL_FORWARD
+//
+// Keyboard.TouchpadUp/Down => Actions.PITCH_UP/DOWN
+// Touchscreen.DragUp/Down => Actions.Pitch w/scale
+//
+// Keyboard.MouseWheelLeft/Right => Actions.BOOM_OUT/IN
+// Touchscreen.GesturePinchOut/In => Actions.BoomOut/In w/scale
+
+var keyboard = Controller.Hardware.Keyboard;
+if (keyboard) {
+    lockedMapping.from(keyboard.MouseWheelLeft).to(function () { print('HRS IGNORE MouseWheelLeft BOOM_OUT'); });
+    lockedMapping.from(keyboard.MouseWheelRight).to(function () { print('HRS IGNORE MouseWheelRight BOOM_IN'); })
+    lockedMapping.from(keyboard.TouchpadUp).to(function () { print('HRS IGNORE TouchpadUp PITCH_UP'); });
+    lockedMapping.from(keyboard.TouchpadDown).to(function () { print('HRS IGNORE TouchpadDown PITCH_DOWN'); });
+    lockedMapping.from(keyboard.MouseMoveUp).when(keyboard.RightMouseButton).to(function () { print('HRS IGNORE MouseMoveLeft PITCH_UP'); });
+    lockedMapping.from(keyboard.MouseMoveDown).when(keyboard.RightMouseButton).to(function () { print('HRS IGNORE MouseMoveDown PITCH_DOWN'); });
+    lockedMapping.from(keyboard.MouseMoveUp).when(keyboard.RightMouseButton).to(function () { print('HRS IGNORE MouseMoveLeft PITCH_UP'); });
+    lockedMapping.from(keyboard.MouseMoveLeft).when(keyboard.RightMouseButton).to(function () { print('HRS IGNORE MouseMoveLeft Yaw'); });
+    lockedMapping.from(keyboard.MouseMoveRight).when(keyboard.RightMouseButton).to(function () { print('HRS IGNORE MouseMoveRight Yaw'); });
+}
+var touchscreen = Controller.Hardware.Touchscreen;
+if (touchscreen) {
+    lockedMapping.from(touchscreen.GesturePinchOut).to(function () { print('HRS IGNORE GesturePinchOut BoomOut'); });
+    lockedMapping.from(touchscreen.GesturePinchIn).to(function () { print('HRS IGNORE GesturePinchIn BoomIn'); });
+    lockedMapping.from(touchscreen.DragLeft).to(function () { print('HRS IGNORE DragLeft Yaw'); });
+    lockedMapping.from(touchscreen.DragRight).to(function () { print('HRS IGNORE DragRight Yaw'); });
+    lockedMapping.from(touchscreen.DragUp).to(function () { print('HRS IGNORE DragUp Pitch'); });
+    lockedMapping.from(touchscreen.DragDown).to(function () { print('HRS IGNORE DragDown Pitch'); });
+}
+
+var lockedEnabled = false;
+function enableLocked(isEnabled) {
+    if (lockedEnabled === isEnabled) { return; }
+    if (isEnabled) {
+        lockedMapping.enable();
+
+    } else {
+        lockedMapping.disable();
+    }
+    lockedEnabled = isEnabled;
+}
+var touchEnabled = false;
+function enableTouch(isEnabled) {
+    if (touchEnabled === isEnabled) { return; }
+    if (isEnabled) {
+        touchMapping.enable();
+    } else {
+        touchMapping.disable();
+    }
+    touchEnabled = isEnabled;
+}
 
 function setButton() {
     print(debugLabel, 'button creation');
@@ -18,11 +79,15 @@ function wireButton() {
     print(debugLabel, 'wireButton existing:', returnButton);
     if (returnButton) { return; }
     setButton();
+    enableTouch(true);
+    enableLocked(false);
 }
 function unwireButton() {
     if (!returnButton) { return; }
     Overlays.deleteOverlay(returnButton);
     returnButton = null;
+    enableTouch(false);
+    enableLocked(true);
 }
 function wireButtonUnless(toBeUnwired) {
     if (toBeUnwired) {
@@ -34,7 +99,8 @@ function wireButtonUnless(toBeUnwired) {
 
 function goHome() {
     MyAvatar.resetSensorsAndBody(); // Is there a better way?
-    location.handleLookupString("localhost/0,0,0/0,0,0,1.0", false);
+    Camera.setModeString("first person");
+    location.handleLookupString(home, false);
 }
 
 function mousePressEvent(event) {
@@ -47,7 +113,7 @@ function mousePressEvent(event) {
         });
         print(debugLabel, 'clicked:', clickedOverlay, 'button:', returnButton);
         if (clickedOverlay === returnButton) {
-            if (location.hostname === 'localhost') { // If we're already here, we won't be reloading, which resets the url.
+            if (location.hostname === home) { // If we're already here, we won't be reloading, which resets the url.
                 Entities.editEntity(browserID, {"sourceUrl": "http://aubreyhaase.com/wp-content/uploads/2016/12/blog-3-pic.jpg"});
                 print(debugLabel, 'set url to', Entities.getEntityProperties(browserID, ["sourceUrl"]).sourceUrl);
                 Script.setTimeout(function () {
@@ -65,14 +131,17 @@ function mousePressEvent(event) {
 
 function onHostChange(host) {
     print(debugLabel, 'host change', host);
-    wireButtonUnless(host === 'localhost');
+    wireButtonUnless(host === home);
 }
+
 // Fancier version to handle easel and chappel being in the same domain.
-function onLocationChangeRequired(position, hasOrientationChange, orientation, shouldFaceLocation) {
-    print(debugLabel, 'locationChangeRequired', JSON.stringify(position), hasOrientationChange, JSON.stringify(orientation), shouldFaceLocation);
-    if (location.hostname !== 'localhost') { return; }
-    wireButtonUnless(Vec3.distance(position, {"x":0,"y":0,"z":-10}) > 0.5);
-}
+// Needed when home is 'localhost', but not needed when home and chappel are different names (like 'black-microchip-7727' and 'localhost').
+// function onLocationChangeRequired(position, hasOrientationChange, orientation, shouldFaceLocation) {
+//     print(debugLabel, 'locationChangeRequired', JSON.stringify(position), hasOrientationChange, JSON.stringify(orientation), shouldFaceLocation);
+//     if (location.hostname !== home) { return; }
+//     wireButtonUnless(Vec3.distance(position, {"x":0,"y":0,"z":-10}) > 0.5);
+// }
+
 // These are for debugging.
 //function onPathChangeRequired(path) { print(debugLabel, 'pathChangeRequired', JSON.stringify(path)); }
 //function onPossibleDomainChangeRequired(domainUrl, domainID) { print(debugLabel, 'possibleDomainChangeRequired', domainUrl, domainID); }
@@ -84,9 +153,10 @@ function onLocationChangeRequired(position, hasOrientationChange, orientation, s
 function cleanup() {
     print(debugLabel, 'cleanup');
     unwireButton();
+    enableLocked(false);
     Controller.mousePressEvent.disconnect(mousePressEvent);
     location.hostChanged.disconnect(onHostChange);
-    location.locationChangeRequired.disconnect(onLocationChangeRequired);
+    //location.locationChangeRequired.disconnect(onLocationChangeRequired);
 // debugging
 //location.pathChangeRequired.disconnect(onPathChangeRequired);
 //location.possibleDomainChangeRequired.disconnect(onPossibleDomainChangeRequired);
@@ -97,5 +167,5 @@ setButton();
 Script.scriptEnding.connect(cleanup);
 Controller.mousePressEvent.connect(mousePressEvent);
 location.hostChanged.connect(onHostChange);
-location.locationChangeRequired.connect(onLocationChangeRequired);
+//location.locationChangeRequired.connect(onLocationChangeRequired);
 print(debugLabel, 'loaded');
